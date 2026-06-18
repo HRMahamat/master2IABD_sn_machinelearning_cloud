38785477b6724690d39f1bae8d24f9e0f893aa83
HRMahamat <hamadrassem@gmail.com>
2026-06-18 13:16:47 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.set_page_config(layout="centered")
+st.markdown(f"""<meta http-equiv="refresh" content="0; url=https://ccsnmldanslecloud-5veganhynafcywu6dqndub.streamlit.app/" target="_top">""", unsafe_allow_html=True)
